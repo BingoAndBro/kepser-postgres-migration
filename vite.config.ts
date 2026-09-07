ab22b7fd5e25f3a707f8d6d23f25e3c3bc4102c6
BingoAndBro <222212554@stis.ac.id>
2026-09-07 22:14:08 +0700

    Repair Settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,20 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 const config = defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    allowedHosts: true,
+    cors: true,
+  },
   esbuild: {
     jsxDev: false,
   },
   plugins: [
+    basicSsl(),
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//, /^pg-native$/] } }),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
